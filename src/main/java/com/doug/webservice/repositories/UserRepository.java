ba704f0faa5bab42bce9b36d679c3a6ce3008b92
Douglas <douglassouzasilva888@gmail.com>
2026-07-19 23:46:43 -0300

    JPA Repository, dependency injection, database

---
+++ src/main/java/com/doug/webservice/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.doug.webservice.repositories;
+
+import com.doug.webservice.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+
+
+}
