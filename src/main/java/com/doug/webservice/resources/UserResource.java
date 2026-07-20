b31e86df047e544e27fe23d2f58507b8413d4119
Douglas <douglassouzasilva888@gmail.com>
2026-07-20 12:15:38 -0300

    Service layer, component regostration

--- src/main/java/com/doug/webservice/resources/UserResource.java
+++ src/main/java/com/doug/webservice/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.doug.webservice.resources;
 
 import com.doug.webservice.entities.User;
+import com.doug.webservice.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u =new User(1L, "Douglas", "doug@gmail.com", "(11)-977383167", "1234");
+    @Autowired
+    private UserService service;
 
-        return ResponseEntity.ok().body(u);
+//Retorna todos os usuários listados
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+// Retorna o usuário pelo ID
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return  ResponseEntity.ok().body(obj);
     }
 
 }
